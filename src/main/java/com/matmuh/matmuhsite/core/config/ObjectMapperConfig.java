215bb64a0e64ddbbc847517b5474894f894df587
yustyy <yusuf.acmaci@hotmail.com>
2026-03-07 00:06:07 +0300

    refactor: inject ObjectMapper dependency into CustomAuthenticationEntryPointHandler

---
+++ src/main/java/com/matmuh/matmuhsite/core/config/ObjectMapperConfig.java
@@ -0,0 +1,20 @@
+package com.matmuh.matmuhsite.core.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+        mapper.disable(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+
+
+}
